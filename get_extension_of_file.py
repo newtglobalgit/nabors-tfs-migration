287024b30aa066911697931b496c150e830a99d1
ujjawalga <117374144+ujjawalga@users.noreply.github.com>
2023-03-24 15:45:15 +0530

    Added code to migrate branches along with commit history

--- get_extension_of_file.py
+++ get_extension_of_file.py
@@ -9,6 +9,8 @@ extension_counts = {}
 # list to store the files without any extension
 no_extension_files = []
 
+
+def get_file_extension_details()
 input_file = input("Enter the input file name: ")
 with open(input_file, "r") as file:
     for line in file:
@@ -26,7 +28,7 @@ with open(input_file, "r") as file:
             no_extension_files.append(line.strip())
 
 
-output_file = input("Enter the output file name: ")
+output_file = "File_Extension.txt"
 with open(output_file, "w") as file:
     file.write("Files grouped by extension:\n")
     for extension, files in extension_files.items():
