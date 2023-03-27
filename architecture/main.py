de7b74436fb43e3a4367ba8e4047141d29289c6e
Jayakarthi Dhanabalan <jayakarthid@newtglobalcorp.com>
2023-03-27 15:08:02 +0530

    update: Architecture diagram updated

--- architecture/main.py
+++ architecture/main.py
@@ -1,4 +1,5 @@
 from diagrams import Diagram, Cluster
+from diagrams.custom import Custom
 from diagrams.c4 import Container, SystemBoundary, Relationship
 
 graph_attr = {
@@ -14,28 +15,27 @@ with Diagram("TFS Migration - Architecture", direction="LR", graph_attr=graph_at
                     technology="On-premise SCM / Working boards",
                     description="Manages the projects on Team Foundation Server version 2013",
                 )
-        with Cluster("Transition"):
-            with Cluster("Azure Cloud"):
-                with SystemBoundary("Azure DevOps"):
-                    boards = Container(
-                        name="Azure Boards",
-                        technology="Azure DevOps",
-                        description="Azure DevOps account which manages the Work Items on projects basis migration from Team Foundation Server 2013",
-                    )
+        with Cluster("Azure Cloud"):
+            with SystemBoundary("Azure DevOps"):
+                boards = Container(
+                    name="Azure Boards",
+                    technology="Azure DevOps",
+                    description="Azure DevOps account which manages the Work Items on projects basis migration from Team Foundation Server 2013",
+                )
 
-            with Cluster("Source Code Management"):
-                with Cluster("GitHub"):
-                    with SystemBoundary("Source Control"):
-                        github_lfs = Container(
-                            name="LFS",
-                            technology="Large file storage",
-                            description="large file storage solution which manages the source code binaries and ascii on projects migration from Team Foundation Server 2013",
-                        )
-                        github = Container(
-                            name="Repository",
-                            technology="SCM",
-                            description="Source code management solution which manages the source code on projects migration from Team Foundation Server 2013",
-                        )
+        with Cluster("Source Code Management"):
+            with Cluster("GitHub"):
+                with SystemBoundary("Source Control"):
+                    github_lfs = Container(
+                        name="LFS",
+                        technology="Large file storage",
+                        description="large file storage solution which manages the source code binaries and ascii on projects migration from Team Foundation Server 2013",
+                    )
+                    github = Container(
+                        name="Repository",
+                        technology="SCM",
+                        description="Source code management solution which manages the source code on projects migration from Team Foundation Server 2013",
+                    )
 
     github << Relationship("Source code migration") << TFS >> Relationship("Work Item migration") >> boards
     github_lfs << Relationship("Binary / ASCII file migration") << TFS
