c908aef925408e494d4d6d074d7444f1428829ce
jayakarthi <jayakarthi.d@gmail.com>
2023-04-21 20:27:41 +0530

    Demo dir

---
+++ Demo/Discovery_script_dir/test.py
@@ -0,0 +1,34 @@
+from github import Github
+import pandas as pd
+
+def get_file_structure_to_excel(access_token, repo_name, branch_name,user_name):
+    g = Github(access_token)
+    repo = g.get_repo(f'{user_name}/{repo_name}')
+    file_structure = pd.DataFrame(columns=['Directory', 'File_Name', 'File_Type', 'Size(Byte)'])
+    contents = repo.get_contents("", ref=branch_name)
+    while contents:
+        file_content = contents.pop(0)
+        if file_content.type == "dir":
+            if file_content.name == ".git":
+                continue
+            contents.extend(repo.get_contents(file_content.path, ref=branch_name))
+        else:
+            if file_content.name in [".gitattributes", ".git"]:
+                continue
+            extension = file_content.name.split(".")[-1] if "." in file_content.name else ""
+            directory = "/".join(file_content.path.split("/")[:-1])
+            if not directory:
+                directory_path = branch_name
+            else:
+                directory_path = branch_name+"/"+directory
+            file_structure = file_structure.append({
+                'Directory': directory_path,
+                'File_Name': file_content.name,
+                'File_Type': "."+extension,
+                'Size(Byte)': file_content.size
+            }, ignore_index=True)
+
+    file_structure.to_excel("file_structure_{}.xlsx".format(branch_name), index=False)
+
+
+get_file_structure_to_excel("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", "CatCore", "cleanup","UjjAwal021")
---
+++ Demo/Migration_demo_dir/main.py
@@ -0,0 +1,75 @@
+import os, shutil, subprocess, library, warnings, upload_github_actions
+from credentials import cred, server, path, server_urls, projects
+
+user = cred.get('USER')
+cwd = os.getcwd()
+csv_file_path = cwd + "\\extension.csv"
+regex_pattern_file_path = cwd + "\\regex_pattern.csv"
+password = cred.get('PASSWORD')
+url = server.get('host')
+git_user = cred.get('owner')
+git_pwd = cred.get('token')
+server_url = server_urls.get('http_url')
+projects_with_path = library.get_list_of_branches()
+flag = 'True'
+
+def migration(project, path, output_file):
+    warnings.filterwarnings("ignore")
+    global flag
+    branch_name = path.split('/')[-1]
+    defpath = os.path.join("C:\\", "Demo", project, branch_name)
+
+    cmd1 = f'git tfs clone "{server_url}DefaultCollection" {path} "{defpath}" --username "{user}" --password "{password}"'
+    # cmd2 = 'git remote remove origin'
+    cmd3 = f'git remote add origin https://{git_user}:{git_pwd}@github.com/{git_user}/{project}.git'
+    cmd4 = f'git checkout -b {branch_name}'
+    cmd5 = f'git push -u origin {branch_name}'
+
+    try:
+        a=subprocess.run(cmd1, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error cloning TFS repository: {e}")
+        return
+    if flag == 'True':
+        library.source_list_of_files(defpath, output_file)
+        flag = 'False'
+    
+    # upload_github_actions.upload_github_actions(defpath)
+
+    os.chdir(defpath)
+    # try:
+    #     subprocess.run(cmd2, shell=True, check=True)
+    # except subprocess.CalledProcessError as e:
+        # print(f"Error removing origin remote: {e}")
+    try:
+        subprocess.run(cmd3, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error adding GitHub remote: {e}")
+        return
+    try:
+        subprocess.run(cmd4, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error creating branch: {e}")
+        return
+    try:
+        subprocess.run(cmd5, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error pushing branch to GitHub: {e}")
+        return
+    # upload_github_actions.git_push(defpath, branch_name)
+    
+    library.upload_binary_to_git_lfs(defpath, csv_file_path, branch_name)
+    library.upload_regex_binary_to_git_lfs(defpath, regex_pattern_file_path, branch_name)
+    # shutil.rmtree(defpath, ignore_errors=True)
+
+for project, paths in projects_with_path.items():
+    warnings.filterwarnings("ignore")
+    if project == projects.get('project5'):
+        library.create_repo(project)
+        output_file = "Source_repo_info"
+        for path in paths:
+            print(f"Migration for {path}")
+            migration(project, path, output_file)
+
+library.clone_target_git()
+
